6a3c7509432a2204bfabe175ef0dc4a6d10fceb4
igor.solovyev <jwebdom@gmail.com>
2018-12-18 08:50:55 +0300

    Init

---
+++ FaceDetector/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Net;
+
+namespace FaceDetector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(621, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerAsync( 3000 );
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ FaceDetector/Form1.cs
@@ -0,0 +1,168 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceDetector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork( object sender, DoWorkEventArgs e )
+        {
+            // Do not access the form's BackgroundWorker reference directly.
+            // Instead, use the reference provided by the sender parameter.
+            BackgroundWorker bw = sender as BackgroundWorker;
+
+            // Extract the argument.
+            int arg = ( int ) e.Argument;
+
+            // Start the time-consuming operation.
+            e.Result = TimeConsumingOperation( bw, arg );
+
+            // If the operation was canceled by the user, 
+            // set the DoWorkEventArgs.Cancel property to true.
+            if ( bw.CancellationPending )
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private int TimeConsumingOperation( BackgroundWorker bw, int sleepPeriod )
+        {
+            int result = 0;
+
+            Random rand = new Random();
+
+            while ( !bw.CancellationPending )
+            {
+                Thread.Sleep( sleepPeriod );
+                this.pictureBox1.Image = GetImage();
+                this.pictureBox1.Width = 621;
+                this.pictureBox1.Height = 426;
+                this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+
+            return result;
+        }
+
+        private Image GetImage()
+        {
+            byte[] imageArray;
+            using ( var webClient = new WebClient() )
+            {
+                imageArray = webClient.DownloadData( "http://10.0.0.53/webcapture.jpg?command=snap&channel=1&user=admin&password=JYvL3uhC" );
+            }
+
+            Image image;
+            using ( var ms = new MemoryStream( imageArray ) )
+            {
+                image = Image.FromStream( ms );
+            }
+
+            List<Face> faces = MakeAnalysisRequest( imageArray );
+            foreach ( var face in faces )
+            {
+                using ( Graphics g = Graphics.FromImage( image ) )
+                {
+                    Color customColor = Color.FromArgb( 100, Color.Yellow );
+                    Pen pen = new Pen( customColor, 2 );
+                    g.DrawRectangle( pen, new Rectangle( 
+                        face.FaceRectangle.Left, 
+                        face.FaceRectangle.Top,
+                        face.FaceRectangle.Width,
+                        face.FaceRectangle.Height ) );
+                }
+            }
+
+            return image;
+        }
+
+        const string subscriptionKey = "8ffd363207ad475ca0436d985f9614db";
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        private List<Face> MakeAnalysisRequest( byte[] byteData )
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add( "Ocp-Apim-Subscription-Key", subscriptionKey );
+
+            // Request parameters. A third optional parameter is "details".
+            string requestParameters = "returnFaceId=true&returnFaceLandmarks=false" +
+                "&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses," +
+                "emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+            List<Face> faces = new List<Face>();
+            using ( ByteArrayContent content = new ByteArrayContent( byteData ) )
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json"
+                // and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue( "application/octet-stream" );
+
+                // Execute the REST API call.
+                response = client.PostAsync( uri, content ).GetAwaiter().GetResult();
+
+                // Get the JSON response.
+                string contentString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                try
+                {
+                    faces = JsonConvert.DeserializeObject<List<Face>>( contentString );
+                } catch ( Exception ) { }
+            }
+
+            return faces;
+        }
+
+        static byte[] GetImageAsByteArray( string imageUrl )
+        {
+            using ( var webClient = new WebClient() )
+            {
+                return webClient.DownloadData( imageUrl );
+            }
+        }
+
+        private class Face
+        {
+            [JsonProperty( "faceId" )]
+            public string FaceId { get; set; }
+
+            [JsonProperty( "faceRectangle" )]
+            public FaceRectangle FaceRectangle { get; set; }
+        }
+
+        private class FaceRectangle
+        {
+            [JsonProperty( "width" )]
+            public int Width { get; set; }
+            [JsonProperty( "height" )]
+            public int Height { get; set; }
+            [JsonProperty( "left" )]
+            public int Left { get; set; }
+            [JsonProperty( "top" )]
+            public int Top { get; set; }
+
+        }
+
+    }
+}
